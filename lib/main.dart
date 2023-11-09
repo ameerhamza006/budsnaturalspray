ed92466870fb6106eb453655576be6300dbdc867
Ameer Hamza <ameerkhan1134@gmail.com>
2023-11-09 06:07:34 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,73 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
 import 'webview_screen.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
-void main() {
-  runApp(const MyApp());
+
+Future<void> main() async {
+  // Check internet connection with singleton (no custom values allowed)
+  await execute(InternetConnectionChecker());
+
+  // Create customized instance which can be registered via dependency injection
+  final InternetConnectionChecker customInstance =
+  InternetConnectionChecker.createInstance(
+    checkTimeout: const Duration(seconds: 1),
+    checkInterval: const Duration(seconds: 1),
+  );
+
+  // Check internet connection with created instance
+  await execute(customInstance);
 }
+
+Future<void> execute(
+    InternetConnectionChecker internetConnectionChecker,
+    ) async {
+  // Simple check to see if we have Internet
+  // ignore: avoid_print
+  print('''The statement 'this machine is connected to the Internet' is: ''');
+  final bool isConnected = await InternetConnectionChecker().hasConnection;
+  // ignore: avoid_print
+  print(
+    isConnected.toString(),
+  );
+  // returns a bool
+
+  // We can also get an enum instead of a bool
+  // ignore: avoid_print
+  print(
+    'Current status: ${await InternetConnectionChecker().connectionStatus}',
+  );
+  // Prints either InternetConnectionStatus.connected
+  // or InternetConnectionStatus.disconnected
+
+  // actively listen for status updates
+  final StreamSubscription<InternetConnectionStatus> listener =
+  InternetConnectionChecker().onStatusChange.listen(
+        (InternetConnectionStatus status) {
+      switch (status) {
+        case InternetConnectionStatus.connected:
+        // ignore: avoid_print
+          runApp(const MyApp());
+          print('Data connection is available.');
+          break;
+        case InternetConnectionStatus.disconnected:
+        // ignore: avoid_print
+          runApp(const MyAppint());
+          print('You are disconnected from the internet.');
+          break;
+      }
+    },
+  );
+
+  // close listener after 30 seconds, so the program doesn't run forever
+  await Future<void>.delayed(const Duration(seconds: 5));
+  await listener.cancel();
+}
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,6 +78,150 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: WebviewScreen(),
+    );
+  }
+}
+
+
+class MyAppint extends StatelessWidget {
+  const MyAppint({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'No Internet Connection'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      //appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+      //  backgroundColor: Colors.green,
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+       // title: Center(child: Text(widget.title)),
+     // ),
+      body: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/nointernet.png',
+                width: 150,
+              ),
+
+              const Text(
+                'Whoops',
+                  style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold)
+              ),
+              const Text(
+                'No internet connection found Check your connection',
+                  textAlign: TextAlign.center,
+                  style: TextStyle( fontSize: 25,fontWeight: FontWeight.bold)
+              ),
+              TextButton(
+                style: ButtonStyle( padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(50, 10, 50, 10)),foregroundColor: MaterialStateProperty.all(Colors.white), backgroundColor: MaterialStateProperty.all(Colors.green)),
+                onPressed: _incrementCounter,
+                child: Text('Try Again',style: TextStyle( fontSize: 20),),
+              ),
+             // Text('$_counter',
+               // style: Theme.of(context).textTheme.headlineMedium,
+              //),
+            ],
+          ),
+        ),
+      ),
+
+     // floatingActionButton: FloatingActionButton(onPressed: _incrementCounter,
+     //   tooltip: 'Increment',
+      //  child: const Icon(Icons.add),
+     // ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class WebviewScreen extends StatefulWidget {
   const WebviewScreen({super.key});
@@ -12,6 +16,17 @@ class _WebviewScreenState extends State<WebviewScreen> {
 
   late final WebViewController _controller;
 
+  arrowback() async{
+    if(await _controller.canGoBack()){
+      await _controller.canGoBack();
+    }
+  }
+  arrowForword() async{
+    if(await _controller.canGoForward()){
+      await _controller.canGoForward();
+    }
+  }
+
   @override
   void initState(){
     super.initState();
@@ -20,13 +35,28 @@ class _WebviewScreenState extends State<WebviewScreen> {
   final WebViewController controller = WebViewController()
   ..setJavaScriptMode(JavaScriptMode.unrestricted)
   ..setBackgroundColor(Colors.white)
+
   ..loadRequest(Uri.parse('https://budsnaturalspray.com/mobile-home/'));
   _controller = controller;
   }
 
 
   @override
-  Widget build(BuildContext context) {
-    return WebViewWidget(controller: _controller,);
-  }
+  Widget build(BuildContext context) => WillPopScope(
+    child:  WebViewWidget(controller: _controller,),
+    onWillPop: () async {
+      if(await _controller.canGoBack()){
+         await _controller.canGoBack();
+        return false;
+      }else{
+        return true;
+      }
+
+    },
+  );
+  //Widget build(BuildContext context) {
+
+    //return WebViewWidget(controller: _controller,);
+
+  //}
 }
